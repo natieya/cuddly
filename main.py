80336fe8289a07ff4ed47634a70e158d97696b8f
n3xtChanc3 <starsonthesky@protonmail.com>
2023-11-21 20:43:42 +0100

    Add README and update .gitignore and some new added files :)

--- main.py
+++
@@ -1,6 +0,0 @@
-def greet(name):
-	print(f"Hello {name}")
-
-greet("Zdravko")
-
-greet("I am sorry")
---
+++ models.py
@@ -0,0 +1,25 @@
+# models.py
+
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from db import db
+
+#db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+    	return f"Post('{self.title}', '{self.timestamp}')"
